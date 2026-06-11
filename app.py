da3c6cb08f4621ca6a7cf0ee82f4507ef1bcfd9d
Amit singh <96075078+Amitsingh-nsut2027@users.noreply.github.com>
2026-06-11 19:16:24 +0530

    Enhance comments in app.py for better understanding
    Updated comments for clarity and improved readability.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#python lib....
 import streamlit as st
 import pandas as pd
 import plotly.express as px
@@ -6,7 +7,7 @@ df = pd.read_csv("zomato_cleaned.csv")
 
 st.title("Zomato Bangalore Dashboard")
 
-# Sidebar Filters
+# Sidebar Filters basics
 st.sidebar.header("Filters")
 locations = ["All"] + sorted(df['location'].unique().tolist())
 selected_location = st.sidebar.selectbox("Select Location", locations)
@@ -14,7 +15,7 @@ selected_location = st.sidebar.selectbox("Select Location", locations)
 if selected_location != "All":
     df = df[df['location'] == selected_location]
 
-# KPI Cards
+# KPI Cards from filterd data 
 col1, col2, col3 = st.columns(3)
 col1.metric("Total Restaurants", len(df))
 col2.metric("Average Rating", round(df['rate'].mean(), 2))
@@ -25,7 +26,7 @@ col3.metric("Avg Cost for Two", f"₹{int(df['cost_for_two'].mean())}")
 # st.dataframe(df[['name', 'location', 'rate', 'cost_for_two', 'cuisines']].reset_index(drop=True))
 
 
-# Charts
+# Charts for visualization
 col1, col2 = st.columns(2)
 
 with col1:
